d0cfd08216ea8a9b3a01e78932921266a70a2115
dangerye <dangerye99@gmail.com>
2019-05-17 16:52:47 +0800

    project Initialization

---
+++ src/main/java/com/dangerye/games/aias/AntiqueInfernalAffairsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dangerye.games.aias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiqueInfernalAffairsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntiqueInfernalAffairsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dangerye/games/aias/AntiqueInfernalAffairsServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dangerye.games.aias;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AntiqueInfernalAffairsServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
